5ed9b82eee381f0b49a964c40a0840714a620109
Shai Nagar <sha1n@users.noreply.github.com>
2021-09-12 02:26:32 +0300

    basic ci setup for testing (#9)

---
+++ tests/unit.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env zsh
+
+local script_dir=${0:a:h}
+local test_home=$(mktemp -d)
+local original_path="$PATH"
+
+unset PATH_ETHIC_HEAD
+unset PATH_ETHIC_TAIL
+
+# Make sure we don't interact witn the local user home
+export HOME=$test_home
+
+# Source the plugin (this won't load the plugin!)
+source $script_dir/../path-ethic.plugin.zsh
+
+###############################################################################
+
+function unit_reset() {
+  export PATH_ETHIC_HEAD=""
+  export PATH_ETHIC_TAIL=""
+  export PATH="$original_path"
+}
+
+function header() {
+  printf " > TESTING: %s...\n" $1
+  
+  unit_reset
+}
+
+function existing_dir_from() {
+  mkdir -p "$1"
+  echo "$1"
+}
+
+function assert_equals() {
+  if [[ "$1" != "$2" ]]; then
+    print "NOT EQUAL!
+
+Expected: '$2'
+
+Actual: '$1'
+"
+
+    exit 1
+  fi
+}
